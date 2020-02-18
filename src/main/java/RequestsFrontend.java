28b65efa858dcafc14ea1fe03ecd79ecbbc92a9d
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-02-18 19:37:22 +0200

    initial

---
+++ src/main/java/RequestsFrontend.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * todo provide framework for measurement of time
+ * required to finish with a portions of requests
+ * */
+
+public class RequestsFrontend {
+
+    public void nextPageReceived()
+    {
+
+    }
+
+    private ArrayList<Integer> reqSlidingWindow;
+}
