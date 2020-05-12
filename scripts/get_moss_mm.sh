099cf35ce4861505a958577de862a0a33908bd31
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-05-13 01:24:25 +0300

    Fix integration of MOSS, now everything builds.

--- scripts/get_moss_mm.sh
+++ scripts/get_moss_mm.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-rm -rf ../external/moss
-mkdir -p ../external/moss
-cd ../external/moss
-wget www.ontko.com/moss/memory/memory.tgz
-tar -xf memory.tgz
+# run from 'project_root'/scripts
+top_dir=$(cd ..; pwd)
+downloaded=${top_dir}/external/moss
+destination=${top_dir}/src/main/java
 
+rm -rf $downloaded && \
+    mkdir -p $downloaded
+
+cd $downloaded && \
+    wget www.ontko.com/moss/memory/memory.tgz && \
+    tar -xf memory.tgz
+
+# remove imports that cause compilation errors
 find . -name '*.java' -exec sed -i '/^import [A-Z]/d' {} +
+
+cd $top_dir/external/moss && \
+    cp -f *.java $destination
